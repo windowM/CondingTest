6426a8eb6897666fef572ac9bf31e104719969b8
MoonChangHo <https://github.com/MoonChangHo/project1.git>
2023-01-08 22:56:02 +0900

    3 comit

---
+++ chapter4/Problem4_2.py
@@ -0,0 +1,12 @@
+###시각 : 3이 하나라도 포함되는 경우의 수를 구해라
+N=int(input())
+count=0
+for i in range(N+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i)+str(j)+str(k):
+                count+=1
+
+print(count)
+
+
